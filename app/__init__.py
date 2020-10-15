27bd07fa4e2eba603c06cfbd6ae3d09ea1f88d60
Victor Santos Silva <victor.s.silva@unesp.br>
2020-10-15 20:24:00 -0300

    add bootstrap in index.html template

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 Migrate = Migrate(app, db)
 manager = Manager(app)
---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
